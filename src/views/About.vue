8d62416ab2ace915f6ddde9697bfcf9af1509a12
常红彬 <email@example.com>
2022-04-24 09:15:00 +0800

    fix: a bug fix

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <ul>
+      <li>
+        <a href="https://github.com/vu" target="_blank" rel="noopener">babel</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
+          target="_blank"
+          rel="noopener"
+        >
+          router
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
+          target="_blank"
+          rel="noopener"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+          target="_blank"
+          rel="noopener"
+        >
+          eslint
+        </a>
+      </li>
+      <li>
+        <a href="https://waagertwqetqwrtrgegrarg/argesrgregerg/argrgargarg/aergargargarg/aergarg">你好</a>
+      </li>
+    </ul>
   </div>
 </template>
